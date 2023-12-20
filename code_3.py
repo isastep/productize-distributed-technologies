6cc92841ca6457b4c1d2da91ea4f25dff619e9ef
Stepan Isayev <140114944+isastep@users.noreply.github.com>
2023-12-20 18:52:27 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,43 @@
+
+import random
+
+if __name__ == "__main__":
+    main()
+    for item in data:
+    return data
+        print(f"Random Number: {item}")
+
+def main():
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+    }
+
+    const data = [];
+main();
+    return data;
+
+function generateRandomData() {
+    data.forEach(item => {
+function main() {
+    });
+        console.log("Random Number: " + item);
+}
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for item in data:
+
+import random
+if __name__ == "__main__":
+    return data
+
+def generate_random_data():
+def main():
+    data = generate_random_data()
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+        print(f"Random Number: {item}")
